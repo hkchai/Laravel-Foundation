555bacc792639760ee9639ca7ca6c90af997a455
HK Chai <hk.chai@gmail.com>
2015-09-13 15:22:18 +0800

    Initial commit

---
+++ Pagination/src/SimpleFoundationFivePresenter.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace hkchai\FoundationFivePagination;
+
+use Illuminate\Contracts\Pagination\Paginator as PaginatorContract;
+
+class SimpleFoundationFivePresenter extends FoundationFivePresenter
+{
+    /**
+     * Create a simple Foundation 5 presenter.
+     *
+     * @param  \Illuminate\Contracts\Pagination\Paginator  $paginator
+     * @return void
+     */
+    public function __construct(PaginatorContract $paginator)
+    {
+        $this->paginator = $paginator;
+    }
+
+    /**
+     * Determine if the underlying paginator being presented has pages to show.
+     *
+     * @return bool
+     */
+    public function hasPages()
+    {
+        return $this->paginator->hasPages() && count($this->paginator->items()) > 0;
+    }
+
+    /**
+     * Convert the URL window into Bootstrap HTML.
+     *
+     * @return string
+     */
+    public function render()
+    {
+        if ($this->hasPages()) {
+            return sprintf(
+                '<ul class="pagination">%s %s</ul>',
+                $this->getPreviousButton(),
+                $this->getNextButton()
+            );
+        }
+
+        return '';
+    }
+}
